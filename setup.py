c271902afd2757d871bb6497c19835d880362279
amgstation <amgstation@naver.com>
2019-02-17 16:06:21 +0900

    setup.py 수정

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import sys
 
-from flask_script	import Mananger
+from flask_script	import Manager
 from app	import create_app
 from flask_twisted import Twisted
 from twisted.python import log
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 	app = create_app()
 	twisted = Twisted(app)
 	log.startLogging(sys.stdout)
-	
+
 	app.logger.info(f"Running the app...")
 	manager = Manager(app)
 	manager.run()
